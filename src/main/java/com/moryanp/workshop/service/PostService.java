94706aa6a110ce515a061fa5f87c5ec153265d08
Natan Morya Paredes <barralagart@gmail.com>
2022-01-13 01:11:40 -0300

    post getById implementation

---
+++ src/main/java/com/moryanp/workshop/service/PostService.java
@@ -0,0 +1,24 @@
+package com.moryanp.workshop.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.moryanp.workshop.domain.Post;
+import com.moryanp.workshop.repository.PostRepository;
+import com.moryanp.workshop.service.exception.ObjectNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id) {
+        Optional<Post> post = repository.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+}
