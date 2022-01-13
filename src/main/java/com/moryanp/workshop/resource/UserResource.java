cde7824fded0a0ec62d1091a9c61cb5dab1b8faa
Natan Morya Paredes <barralagart@gmail.com>
2022-01-12 21:38:34 -0300

    delete user

--- src/main/java/com/moryanp/workshop/resource/UserResource.java
+++ src/main/java/com/moryanp/workshop/resource/UserResource.java
@@ -10,6 +10,7 @@ import com.moryanp.workshop.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,4 +46,11 @@ public class UserResource {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(objDto);
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        service.remove(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
